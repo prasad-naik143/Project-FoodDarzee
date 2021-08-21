584e0a59362727b9ddcbd11084f8131e68005c66
prasadn0143@gmail.com <prasadn0143@gmail.com>
2021-08-22 01:48:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FoodDarzee';
+  img1 = [
+    '../assets/Chef1.jpg',
+    '../assets/Food.jpg',
+    '../assets/Chef3.jpg'
+  ];
+
+  alt1 = ["Chef1",'Food','Chef3'];
+
+  img2 = [
+    '../assets/Chef2.jpg',
+    '../assets/Food2.jpg',
+    '../assets/Orange.jpg'
+  ];
+
+  alt2 = ["Chef1",'Food2','Orange'];
+
+
+  img3 =[
+    '../assets/Coffee2.jpg',
+    '../assets/Coffee3.jpg',
+    '../assets/Chef4.jpg'
+  ];
+
+  alt3 = ["Coffee2",'Coffee3','Chef4'];
+
+  img4 =[
+    '../assets/Passion2.jpg',
+    '../assets/Passion4.jpg',
+    '../assets/Passion3.jpg'
+  ];
+
+  alt4 = ["Passion2",'Passion4','Passion3'];
+
+  img5 =[
+    '../assets/Biriyani.jpg',
+    '../assets/Tracy-adams3.jpg',
+    '../assets/Kitchen3.jpg'
+  ];
+
+  alt5 = ["Biriyani",'Tracy-adams','Kitchen'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+
+      transferArrayItem(event.container.data,    
+                        event.previousContainer.data,
+                        event.currentIndex+1,
+                        event.previousIndex);
+                        
+    }
+  }
 }
